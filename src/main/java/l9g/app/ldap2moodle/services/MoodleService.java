5d624307a5fd84c1f621e62628f4b6078fe14953
Karin Borm <k.borm@ostfalia.de>
2024-04-17 11:36:35 +0200

    send "create user" request to moodle

--- src/main/java/l9g/app/ldap2moodle/services/MoodleService.java
+++ src/main/java/l9g/app/ldap2moodle/services/MoodleService.java
@@ -15,7 +15,9 @@
  */
 package l9g.app.ldap2moodle.services;
 
+import io.netty.handler.logging.LogLevel;
 import java.net.URI;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -33,12 +35,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 /**
  *
@@ -47,10 +54,8 @@ import reactor.core.publisher.Mono;
 @Service
 public class MoodleService
 {
- 
-  
-  private final static Logger LOGGER
-    = LoggerFactory.getLogger(MoodleService.class);
+  private final static Logger LOGGER =
+    LoggerFactory.getLogger( MoodleService.class );
 
   final private RestTemplate restTemplate = new RestTemplate();
 
@@ -59,68 +64,76 @@ public class MoodleService
   final private Config config;
 
   @Autowired
-  public MoodleService(Config config, CryptoHandler cryptoHandler)
+  public MoodleService( Config config, CryptoHandler cryptoHandler )
   {
-    this.wstoken = cryptoHandler.decrypt(config.getMoodleToken());
+    LOGGER.debug( "MoodleService constructor" );
+
+    this.wstoken = cryptoHandler.decrypt( config.getMoodleToken() );
     this.config = config;
   }
 
-  record UserCreateResponse(int id, String username) {}
-  record UsersCreateResponse(List<UserCreateResponse> list) {}  
+  record UserCreateResponse( int id, String username)
+    {
+      }
 
-  record UsersCreateRequest(List<MoodleUser> list) {}    
-  
+  record UsersCreateResponse( List<UserCreateResponse> list)
+    {
+      }
+
+  record UsersCreateRequest( List<MoodleUser> list)
+    {
+      }
+
   /**
    * create URI with parameters
+   *
    * @param wsfunction
    * @param parameters
+   *
    * @return Uri object
    */
-  private URI uriBuilder(String wsfunction,
-    LinkedHashMap<String, String> parameters)
+  private URI uriBuilder( String wsfunction,
+    LinkedHashMap<String, String> parameters )
   {
     UriComponentsBuilder builder = UriComponentsBuilder
-      .fromHttpUrl(config.getMoodleBaseUrl() + "/webservice/rest/server.php")
-      .queryParam("wstoken", wstoken)
-      .queryParam("moodlewsrestformat", "json")
-      .queryParam("wsfunction", wsfunction);
+      .fromHttpUrl( config.getMoodleBaseUrl() + "/webservice/rest/server.php" )
+      .queryParam( "wstoken", wstoken )
+      .queryParam( "moodlewsrestformat", "json" )
+      .queryParam( "wsfunction", wsfunction );
 
-    if (parameters != null && !parameters.isEmpty())
+    if( parameters != null && !parameters.isEmpty() )
     {
-      parameters.forEach((key, value) -> builder.queryParam(key, value));
+      parameters.forEach( ( key, value ) -> builder.queryParam( key, value ) );
     }
 
     UriComponents uriComponents = builder.build().encode();
-    LOGGER.debug("uri={}", uriComponents.toUriString());
+    LOGGER.debug( "uri={}", uriComponents.toUriString() );
 
     return uriComponents.toUri();
   }
 
-
-  
   /**
    * get all users authenticated by LDAP/OICD from Moodle
-   * 
-   * @return 
+   *
+   * @return
    */
   public List<MoodleUser> users()
   {
     List<MoodleUser> result = null;
 
     LinkedHashMap<String, String> criterias = new LinkedHashMap<>();
-    criterias.put("criteria[0][key]", "auth");
-    criterias.put("criteria[0][value]", "ldap");
+    criterias.put( "criteria[0][key]", "auth" );
+    criterias.put( "criteria[0][value]", "ldap" );
 //    criterias.put("criteria[0][key]", "email");
 //    criterias.put("criteria[0][value]", "%");
 
-    
-    ResponseEntity<MoodleUsersResponse> response
-      = restTemplate.getForEntity(
-        uriBuilder("core_user_get_users", criterias),
-        MoodleUsersResponse.class);
+    ResponseEntity<MoodleUsersResponse> response =
+      restTemplate.getForEntity(
+        uriBuilder( "core_user_get_users", criterias ),
+        MoodleUsersResponse.class );
 
-    if (response != null && response.getBody() != null
-      && response.getStatusCode() == HttpStatus.OK)
+    if( response != null && response.getBody() != null
+      && response.getStatusCode() == HttpStatus.OK )
     {
       result = response.getBody().getUsers();
     }
@@ -135,30 +148,33 @@ public class MoodleService
   }
 
   /**
-   * create user in Moodle 
+   * create user in Moodle
+   *
    * @param user object
+   *
    * @return ???
    */
-  public UsersCreateResponse usersCreateOld(MoodleUser user) throws Exception
+  public UsersCreateResponse usersCreateOld( MoodleUser user )
+    throws Exception
   {
     LinkedHashMap<String, String> criteria = new LinkedHashMap<>();
     UsersCreateResponse result = null;
-    
+
     HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    
+    headers.setContentType( MediaType.APPLICATION_JSON );
+
     List<MoodleUser> list = new ArrayList<>();
     list.add( user );
-    UsersCreateRequest payload = new UsersCreateRequest(list);
-    
-    HttpEntity<UsersCreateRequest> request = new HttpEntity<>(payload, headers);
+    UsersCreateRequest payload = new UsersCreateRequest( list );
 
-    ResponseEntity<String> response1
-      = restTemplate.postForEntity(
-        uriBuilder("core_user_create_users", criteria), request,
-        String.class);   
+    HttpEntity<UsersCreateRequest> request = new HttpEntity<>( payload, headers );
+
+    ResponseEntity<String> response1 =
+      restTemplate.postForEntity(
+        uriBuilder( "core_user_create_users", criteria ), request,
+        String.class );
     LOGGER.debug( response1.toString() );
-/*    
+    /*    
     ResponseEntity<UsersCreateResponse> response
       = restTemplate.postForEntity(
         uriBuilder("core_user_create_users", criteria), request,
@@ -173,42 +189,71 @@ public class MoodleService
     {
       throw new Exception("user " + user.getEmail() + " could not be inserted");
     }
-*/
+     */
     return result;
   }
-  
-    
-  
-  
-  public boolean usersCreate(MoodleUser user) throws Exception
+
+  public WebClient webClient()
   {
+    var httpClient = HttpClient
+      .create()
+      .wiretap( "reactor.netty.http.client.HttpClient",
+        LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL );
+
+    return WebClient
+      .builder()
+      .clientConnector( new ReactorClientHttpConnector( httpClient ) )
+      .baseUrl( config.getMoodleBaseUrl() )
+      .build();
+  }
+
+  public boolean usersCreate( MoodleUser user )
+    throws Exception
+  {
+    LOGGER.debug( "usersCreate" );
 
     List<MoodleUser> list = new ArrayList<>();
     list.add( user );
-    UsersCreateRequest payload = new UsersCreateRequest(list);    
-    
-    WebClient webClient = WebClient.create();
-    String path = config.getMoodleBaseUrl() + "/webservice/rest/server.php";
-    LOGGER.debug(path);
-    Mono<ResponseEntity<String>> response = webClient.post()
-      .uri(uriBuilder -> 
-          uriBuilder.path(path)
-            .queryParam("wstoken", wstoken)
-            .queryParam("moodlewsrestformat", "json")
-            .queryParam("wsfunction", "core_user_create_users")
-            .build()
-      )
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .body(Mono.just(payload), UsersCreateRequest.class)      
-      .retrieve()
-      .toEntity(String.class);
+    UsersCreateRequest payload = new UsersCreateRequest( list );
 
+    // Mono<ResponseEntity<String>> 
+    Mono<String> response1 = this.webClient().post()
+      .uri( uriBuilder
+        -> uriBuilder.path( "/webservice/rest/server.php" )
+        .queryParam( "wstoken", wstoken )
+        .queryParam( "moodlewsrestformat", "json" )
+        .queryParam( "wsfunction", "core_user_create_users" )
+        .build() )
+      // .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+      .accept( MediaType.APPLICATION_JSON )
+      .contentType( MediaType.APPLICATION_JSON )
+      //      .body(Mono.just(payload), UsersCreateRequest.class)      
+      .body( BodyInserters.fromValue( payload ) )
+      .exchangeToMono( response ->
+      {
+        LOGGER.info( response.toString() );
+        if( response.statusCode().equals( HttpStatus.OK ) )
+        {
+          LOGGER.info( "OK" );
+          return response.bodyToMono( String.class );
+        }
+        else
+        {
+          // Turn to error
+          LOGGER.info( "not ok" );
+          return response.createError();
+        }
+      } );
+    String body = response1.block();
+    LOGGER.info( body );
+
+//      .toEntity(String.class);
 //      .bodyToMono(String.class);
 //    ResponseEntity<String> block = response.block();
 //    LOGGER.debug( block.toString());
-    LOGGER.debug( response.toString());
+    LOGGER.info( response1.toString() );
 
-/*    
+    /*    
     ResponseEntity<UsersCreateResponse> response
       = restTemplate.postForEntity(
         uriBuilder("core_user_create_users", criteria), request,
@@ -223,20 +268,20 @@ public class MoodleService
     {
       throw new Exception("user " + user.getEmail() + " could not be inserted");
     }
-*/
+     */
     return true;
-  }  
-  
+  }
 
   // TODO: ...
-  public MoodleUser usersUpdate(int id, MoodleUser user)
+  public MoodleUser usersUpdate( int id, MoodleUser user )
   {
     return user;
   }
 
   // TODO: ...
-  public MoodleUser usersAnonymize(int id, MoodleAnonymousUser user)
+  public MoodleUser usersAnonymize( int id, MoodleAnonymousUser user )
   {
     return null;
   }
+
 }
