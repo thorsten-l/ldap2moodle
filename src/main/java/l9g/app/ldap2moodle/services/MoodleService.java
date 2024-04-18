5065f774302632e60dc675d8cfc729c46419be14
Karin Borm <k.borm@ostfalia.de>
2024-04-18 13:33:24 +0200

    create user via URL

--- src/main/java/l9g/app/ldap2moodle/services/MoodleService.java
+++ src/main/java/l9g/app/ldap2moodle/services/MoodleService.java
@@ -18,10 +18,9 @@ package l9g.app.ldap2moodle.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.handler.logging.LogLevel;
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import l9g.app.ldap2moodle.Config;
 import l9g.app.ldap2moodle.handler.CryptoHandler;
@@ -44,11 +43,13 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.transport.logging.AdvancedByteBufFormat;
+import java.net.URLEncoder;
 
 /**
  *
@@ -57,6 +58,37 @@ import reactor.netty.transport.logging.AdvancedByteBufFormat;
 @Service
 public class MoodleService
 {
+  public class MoodleRestException extends Exception {
+
+    public static final String NO_LEGACY="There is no legacy call for this webservice";
+    public static final String AUTH_NULL="No credential provided";
+    public static final String REQUIRED_PARAMETER="Parameter is required";
+    public static final String PARAMETER_RANGE="Parameter in not within an accepted range";
+    public static final String PARAMETER_CANNOT_BE_NULL="Parameter cannot be null";
+    public static final String TOKEN_NULL="Token cannot be null";
+    public static final String USERNAME_NULL="Username cannot be null";
+    public static final String PASSWORD_NULL="Password cannot be null";
+    public static final String FIRSTNAME_NULL="Firstname cannot be null";
+    public static final String LASTNAME_NULL="Lastname cannot be null";
+    public static final String EMAIL_NULL="Email cannot be null";
+    public static final String URL_NULL="URL cannot be null";
+    public static final String USER_NULL="User cannot be null";
+    public static final String INVALID_USERID="Bad user id";
+    public static final String INVALID_USER="Invalid user";
+    public static final String NO_LEGACY_CALL="No legacy call";    
+    
+    MoodleRestException() {}
+
+    MoodleRestException(String msg) {
+      super(msg);
+    }
+
+    @Override
+    public String getMessage() {
+      return super.getMessage();
+    }
+  }
+
   private final static Logger LOGGER =
     LoggerFactory.getLogger( MoodleService.class );
 
@@ -208,26 +240,71 @@ public class MoodleService
       .build();
   }
 
+  // from https://github.com/bantonia/MoodleRest/blob/master/src/net/beaconhillcott/moodlerest/MoodleRestUser.java
+  private String getCreateUri(MoodleUser[] users) throws MoodleRestException // Argh!
+  {
+    // try {
+      StringBuilder data=new StringBuilder();
+      for (int i=0;i<users.length;i++) {
+        if (users[i]==null) throw new MoodleRestException(MoodleRestException.USER_NULL);
+        if (users[i].getUsername()==null) throw new MoodleRestException(MoodleRestException.USERNAME_NULL); else data.append("&").append("users["+i+"][username]").append("=").append(users[i].getUsername());
+        if (users[i].getFirstname()==null) throw new MoodleRestException(MoodleRestException.FIRSTNAME_NULL); else data.append("&").append("users["+i+"][firstname]").append("=").append(users[i].getFirstname());
+        if (users[i].getLastname()==null) throw new MoodleRestException(MoodleRestException.LASTNAME_NULL); else data.append("&").append("users["+i+"][lastname]").append("=").append(users[i].getLastname());
+        if (users[i].getEmail()==null) throw new MoodleRestException(MoodleRestException.EMAIL_NULL); else data.append("&").append("users["+i+"][email]").append("=").append(users[i].getEmail());
+        if (users[i].getAuth()!=null) data.append("&").append("users["+i+"][auth]").append("=").append(users[i].getAuth());
+        if (users[i].getIdnumber()!=null) data.append("&").append("users["+i+"][idnumber]").append("=").append(users[i].getIdnumber());
+        if (users[i].getLang()!=null) data.append("&").append("users["+i+"][lang]").append("=").append(users[i].getLang());
+        if (users[i].getTimezone()!=null) data.append("&").append("users["+i+"][timezone]").append("=").append(users[i].getTimezone());
+        if (users[i].getDescription()!=null) data.append("&").append("users["+i+"][description]").append("=").append(users[i].getDescription());
+        if (users[i].getCountry()!=null) data.append("&").append("users["+i+"][country]").append("=").append(users[i].getCountry());
+//        if (users[i].getAlternatename()!=null) data.append("&").append("users["+i+"][alternatename]").append("=").append(user[i].getAlternatename());
+/*        if (users[i].getCustomFields()!=null) {
+          for (int j=0; j<user[i].getCustomFields().size(); j++) {
+            data.append("&").append("users["+i+"][customfields]["+j+"][type]").append("=").append(user[i].getCustomFields().get(j).getName()));
+            data.append("&").append("users["+i+"][customfields]["+j+"][value]").append("=").append(user[i].getCustomFields().get(j).getValue()));
+          }
+        }*/
+      }
+      data.trimToSize();
+/*
+      NodeList elements=MoodleCallRestWebService.call(data.toString());
+      for (int j=0;j<elements.getLength();j+=2) {
+        hash.put(elements.item(j+1).getTextContent(), elements.item(j).getTextContent());
+      }
+    }  catch (UnsupportedEncodingException ex) {
+      Logger.getLogger(MoodleRestUser.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+ */
+    /*
+    for (int i=0; i<user.length; i++) {
+      user[i].setId(Long.parseLong((String)(hash.get(user[i].getUsername()))));
+    }*/
+
+    return data.toString();
+  }
+
   public boolean usersCreate( MoodleUser user )
     throws Exception
   {
     LOGGER.debug( "usersCreate" );
 
-    List<MoodleUser> users = new ArrayList<>();
-    users.add( user );
-    UsersCreateRequest payload = new UsersCreateRequest( users );
+//    List<HashMap<String, String>> users = new ArrayList<>();
+//    users.add( user.toMap() );
 
-    // Mono<ResponseEntity<String>> 
+    MoodleUser moodleusers[] = new MoodleUser[1];
+    moodleusers[0] = user;
+
+    String query = this.getCreateUri(moodleusers);
     Mono<String> response1 = this.webClient().post()
-      .uri( uriBuilder
-        -> uriBuilder.path( "/webservice/rest/server.php" )
-        .queryParam( "wstoken", wstoken )
-        .queryParam( "moodlewsrestformat", "json" )
-        .queryParam( "wsfunction", "core_user_create_users" )
-        .build() )
+      .uri(uriBuilder
+          -> uriBuilder.path("/webservice/rest/server.php")
+          .queryParam("wstoken", wstoken)
+          .queryParam("moodlewsrestformat", "json")
+          .queryParam("wsfunction", "core_user_create_users")
+          .query(query)
+          .build())
       .accept( MediaType.APPLICATION_JSON )
-      .contentType( MediaType.APPLICATION_JSON )
-      .bodyValue( payload ) // BodyInserters.fromValue( payload ) )
       .exchangeToMono( response ->
       {
         LOGGER.info( response.toString() );
